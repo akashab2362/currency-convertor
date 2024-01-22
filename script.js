aead62aa571c7d5a725e42799b11a70b90a3cf5a
akashab2362 <akashab2362@gmail.com>
2024-01-22 15:11:22 +0530

    Currency Convertor application completed

---
+++ script.js
@@ -0,0 +1,62 @@
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+
+for (let select of dropdowns) {
+  for (let currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if(select.name === "from" && currCode == "USD")
+    {
+        newOption.selected = "selected";
+    }
+    else if(select.name === "to" && currCode === "INR")
+    {
+        newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+  select.addEventListener("change", (evt) =>{
+    updateFlag(evt.target);
+  })
+}
+
+const updateExchangeRate = async ()=>{
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    console.log(amtVal);
+    if(amtVal === "" || amtVal <= 0)
+    {
+        amtVal = 1;
+        amount.value = "1";
+    }
+    console.log(fromCurr.value)
+    console.log(toCurr.value)
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+    let finalAmount = amtVal * rate;
+    document.querySelector(".message").innerHTML = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+}
+
+const updateFlag = (elem)=>{
+    let currCode = elem.value;
+    let countryCode = countryList[currCode];
+    console.log(countryCode);
+    let newSrcLink = `https://flagsapi.com/${countryCode}/shiny/64.png`;
+    let img = elem.parentElement.querySelector("img");
+    img.src = newSrcLink;
+}
+
+btn.addEventListener("click", async (evt)=>{
+    evt.preventDefault();
+    updateExchangeRate();
+});
+window.addEventListener("load", ()=>{
+    updateExchangeRate();
+});
